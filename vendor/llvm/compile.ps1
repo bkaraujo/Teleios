73a66e1171513f5bd2eea8b492d9f4399f531c0a
Bruno da Silva Perez Araujo <bruno.araujo@funpresp.com.br>
2024-07-23 11:34:39 -0300

    Parallel compilation

--- vendor/llvm/compile.ps1
+++ vendor/llvm/compile.ps1
@@ -28,27 +28,27 @@ Set-Location "$TARGETFS"
 # ##############################################################################
 foreach ($folder in Get-ChildItem $Location | Where-Object {$_.PSIsContainer} | Foreach-Object {$_.Name}) {
     $FDir = "$TARGETFS/$folder"
+    Write-Host "[$(Get-Date -Format "dd/MM/yyyy HH:mm K")]   Compiling $FDir"
     
     if (Test-Path -Path "$FDir" ){}
     else { New-Item -ItemType Directory "$FDir" -Force | Out-Null }
     
     Set-Location "$FDir"
-    foreach ($file in Get-ChildItem -Path "$Location/$folder" -Filter "*.c" -Recurse -File) {
-        $Hash = $(Get-FileHash -Path $file -Algorithm SHA1).Hash
-        $ShaFile = "$FDir/$($file.BaseName).sha1"
+    $(Get-ChildItem -Path "$Location/$folder" -Filter "*.c" -Recurse -File) | Foreach-Object -ThrottleLimit 8 -Parallel {
+        $Hash = $(Get-FileHash -Path "$PSItem" -Algorithm SHA1).Hash
+        $ShaFile = "$USING:FDir/$($PSItem.BaseName).sha1"
 
         # Create the shafile for the first execution
         if (Test-Path -Path $ShaFile) {} 
         else { New-Item -ItemType File -Path $ShaFile | Out-Null }
 
         if ($Hash -ne $(Get-Content $ShaFile | Select-Object -First 1)) { 
-            Write-Host "[$(Get-Date -Format "dd/MM/yyyy HH:mm K")]   Compiling $file"
             $Hash > $ShaFile
 
-            "clang -std=c11 -Wall -Werror -march=x86-64 $CFlags $IFlags $DFlags -c $file" > "$FDir/$($file.BaseName).cmd"
+            "clang -std=c11 -Wall -Werror -march=x86-64 $USING:CFlags $USING:IFlags $USING:DFlags -c $PSItem" > "$FDir/$($PSItem.BaseName).cmd"
             # Perform the compilation and store the result code
             $global:LastExitCode = 0;
-            Invoke-Expression -Command "clang -std=c11 -Wall -Werror -march=x86-64 $CFlags $IFlags $DFlags -c $file"
+            Invoke-Expression -Command "clang -std=c11 -Wall -Werror -march=x86-64 $USING:CFlags $USING:IFlags $USING:DFlags -c $PSItem"
             if ($LastExitCode -ne 0) {
                 Remove-Item -Path $ShaFile | Out-Null
                 return 1
